8042eae5060dca448b8eb5b7c35bf2b9e51e0d68
Daniel Fernando Betancur Villada <danielfernandobetancurvillada@Daniels-MacBook-Air.local>
2024-01-17 07:32:03 -0500

    feat: new build

--- .next/server/chunks/249.js
+++ .next/server/chunks/249.js
@@ -202,7 +202,8 @@ __webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __we
 var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([axios__WEBPACK_IMPORTED_MODULE_0__]);
 axios__WEBPACK_IMPORTED_MODULE_0__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = "http://217.196.49.92:3000/api";
+;
 const login = async (email, password)=>{
     try {
         const response = await axios__WEBPACK_IMPORTED_MODULE_0__["default"].post(`${API_URL}/auth/login`, {
